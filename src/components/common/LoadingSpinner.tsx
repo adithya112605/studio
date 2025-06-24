3251e32d25bd976f79921a4cf16670293c3cff74
V Adithya <127156170@sastra.ac.in>
2025-06-24 04:30:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -15,7 +15,7 @@ const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ size = 'md' }) => {
 
   return (
     <dotlottie-player
-      src="https://lottie.host/4a856247-2b38-425a-9409-385a5a5441b0/Vj2s2FYzYg.json"
+      src="https://lottie.host/588824f7-7275-4c31-a049-74a584959445/yF5oL10QfL.json"
       background="transparent"
       speed="1"
       style={sizeMap[size]}
