708d4c157d61ff34a829770d857e27ea5afe2d0b
V Adithya <127156170@sastra.ac.in>
2025-06-26 06:45:20 +0000

    Provide the functionalities for all the buttons, from including the auth

--- src/app/admin/add-hr/page.tsx
+++ src/app/admin/add-hr/page.tsx
@@ -2,14 +2,17 @@
 "use client"
 
 import ProtectedPage from "@/components/common/ProtectedPage";
-import AddSupervisorForm from "@/components/admin/AddSupervisorForm"; // Renamed component
+import AddSupervisorForm from "@/components/admin/AddSupervisorForm";
+import ScrollReveal from "@/components/common/ScrollReveal";
 
-export default function AddSupervisorPage() { // Renamed page function
+export default function AddHrPage() {
   return (
-    // Access typically restricted to IC Head or high-level admin
+    // This page seems to be for IC Heads to add any kind of supervisor.
     <ProtectedPage allowedRoles={['IC Head']}> 
-      <div className="py-8">
-        <AddSupervisorForm />
+      <div className="container mx-auto py-8 px-4 md:px-6 lg:px-8">
+        <ScrollReveal animationInClass="animate-fadeInUp" once={false}>
+          <AddSupervisorForm />
+        </ScrollReveal>
       </div>
     </ProtectedPage>
   );
